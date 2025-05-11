75efc91a045c4e1ba293f62ae49ea7f327c5c4d4
Константин Кудинов <kudinovkostyan@yandex.ru>
2025-05-11 15:52:13 +0600

    Add files via upload

---
+++ Алгоритмизация и программирование/Второй Семестр/АИП/Lab 6/Car.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+//Лабораторная работа по АИП №6.2
+
+public delegate void CleanVehicleDelegate(Vehicle vehicle);
+
+public class Vehicle
+{
+    public int ReleaseYear { get; set; }
+    public string Brand { get; set; }
+    public string Paint { get; set; }
+    public bool NeedsWashing { get; set; }
+
+    public Vehicle(int year, string brand, string paint, bool dirty)
+    {
+        ReleaseYear = year;
+        Brand = brand;
+        Paint = paint;
+        NeedsWashing = dirty;
+    }
+
+    public override string ToString()
+    {
+        return $"{ReleaseYear} {Brand} ({Paint}) - Грязная: {NeedsWashing}";
+    }
+}
+
+class ParkingLot
+{
+    private List<Vehicle> vehicles;
+
+    public ParkingLot()
+    {
+        vehicles = new List<Vehicle>();
+    }
+
+    public void StoreVehicle(Vehicle vehicle)
+    {
+        vehicles.Add(vehicle);
+    }
+
+    public List<Vehicle> RetrieveAll()
+    {
+        return vehicles;
+    }
+}
+
+class Cleaner
+{
+    public void Clean(Vehicle vehicle)
+    {
+        if (vehicle.NeedsWashing)
+        {
+            vehicle.NeedsWashing = false;
+            Console.WriteLine($"Автомобиль {vehicle.Brand} вымыт.");
+        }
+        else
+        {
+            Console.WriteLine($"Автомобиль {vehicle.Brand} уже чист.");
+        }
+    }
+}
+
+class EntryPoint
+{
+    static void Main()
+    {
+        ParkingLot lot = new ParkingLot();
+
+        lot.StoreVehicle(new Vehicle(2020, "Porsche 911", "Красный", false));
+        lot.StoreVehicle(new Vehicle(2012, "Renault Logan", "Синий", false));
+        lot.StoreVehicle(new Vehicle(2021, "Renault Duster", "Серебрянный", true));
+        lot.StoreVehicle(new Vehicle(2020, "Toyota RAV4", "Белый", true));
+
+        Cleaner autoCleaner = new Cleaner();
+        CleanVehicleDelegate cleanerDelegate = new CleanVehicleDelegate(autoCleaner.Clean);
+
+        foreach (var v in lot.RetrieveAll())
+        {
+            Console.WriteLine(v);
+            cleanerDelegate(v);
+            Console.WriteLine();
+        }
+    }
+}
+          
---
+++ Алгоритмизация и программирование/Второй Семестр/АИП/Lab 6/calculations.cs
@@ -0,0 +1,84 @@
+using System;
+//Лаббораторная работа по АИП №6.1 
+
+public delegate int OperationHandler();
+
+class MathProcessor
+{
+    public int A { get; set; }
+    public int B { get; set; }
+
+    public MathProcessor(int a, int b)
+    {
+        A = a;
+        B = b;
+    }
+
+    public int Sum()
+    {
+        return A + B;
+    }
+
+    public int Difference()
+    {
+        return A - B;
+    }
+
+    public int Product()
+    {
+        return A * B;
+    }
+
+    public int Quotient()
+    {
+        if (B == 0)
+        {
+            throw new DivideByZeroException("Нельзя делить на ноль");
+        }
+        return A / B;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MathProcessor obj1 = new MathProcessor(10, 5);
+        MathProcessor obj2 = new MathProcessor(20, 4);
+
+        OperationHandler task1 = () =>
+        {
+            int result1 = obj1.Sum();
+            Console.WriteLine($"Сложение: {result1}");
+
+            int result2 = result1 * obj1.B;
+            Console.WriteLine($"Умножение: {result2}");
+
+            int result3 = result2 / obj1.B;
+            Console.WriteLine($"Деление: {result3}");
+
+            return result3;
+        };
+
+        OperationHandler task2 = () =>
+        {
+            int part1 = obj2.Quotient();
+            Console.WriteLine($"Частное: {part1}");
+
+            int part2 = part1 - obj2.B;
+            Console.WriteLine($"Разность: {part2}");
+
+            int part3 = part2 * obj2.B;
+            Console.WriteLine($"Произведение: {part3}");
+
+            return part3;
+        };
+
+        Console.WriteLine("Результаты первого вычисления:");
+        task1();
+
+        Console.WriteLine("\nРезультаты второго вычисления:");
+        task2();
+    }
+}
+            

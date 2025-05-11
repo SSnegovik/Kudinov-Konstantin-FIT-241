c2c8c56245337dc240a93bfa79768c7b2bce9496
Константин Кудинов <kudinovkostyan@yandex.ru>
2025-05-11 14:52:40 +0600

    Add files via upload

---
+++ Алгоритмизация и программирование/Второй Семестр/АИП/Staples.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+//Лабараторная работа по АИП №3
+
+public class ExpressionValidator
+{
+    private static readonly Dictionary<char, char> MatchingBrackets = new Dictionary<char, char>
+    {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' }
+    };
+
+    public static bool HasValidBrackets(string input)
+    {
+        var stack = new Stack<char>();
+
+        foreach (var ch in input)
+        {
+            if (MatchingBrackets.ContainsKey(ch))
+            {
+                stack.Push(ch);
+            }
+            else if (MatchingBrackets.ContainsValue(ch))
+            {
+                if (stack.Count == 0 || MatchingBrackets[stack.Pop()] != ch)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Введите выражение: ");
+        var userInput = Console.ReadLine();
+
+        if (HasValidBrackets(userInput))
+        {
+            Console.WriteLine("Скобки расставлены правильно.");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка в расстановке скобок.");
+        }
+    }
+}
+
+
